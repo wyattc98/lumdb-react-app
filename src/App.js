2c70cea0761c97cc13d135dbce20932301969949
Wyatt C <cochranwyatt@yahoo.com>
2019-06-03 12:39:05 -0700

    changed exact path in app.js to work on gh-pages.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => (
         </Link>
       </header>
       <Switch>
-        <Route exact path="/lumdb-react-app" component={MoviesList} />
+        <Route exact path="/lumdb-react-app/" component={MoviesList} />
         <Route path="/:id" component={MovieDetail} />
       </Switch>
     </div>
